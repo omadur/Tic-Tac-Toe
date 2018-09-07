cf5d5ed11f68e04ee8584567d3fde9172bdcf13c
Omar Duran <omarduran1989@hotmail.com>
2018-09-06 21:12:13 -0500

    Condicion ganadora
    Probando la condicion cuando la primera fila es la ganadora

---
+++ Tic-Tac-toe-Unity/Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Text[] buttonList;
+    private string playerSide;
+
+
+    private void Awake()
+    {
+        playerSide = "X";
+        SetGameControllerReferenceOnButtons();
+    }
+
+    void SetGameControllerReferenceOnButtons()
+    {
+        for(int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+    }
+
+    public string GetPlayerSide()
+    {
+        return playerSide;
+    }
+
+    public void endTurn()
+    {
+        if(buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
+    }
+}
+
---
+++ Tic-Tac-toe-Unity/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // agregamos para poder interactuar con los botones
+
+public class GridSpace : MonoBehaviour {
+
+    public Button button;
+    public Text buttonText;
+
+    private GameController gamecontroller;
+
+    public void SetSpace()
+    {
+        buttonText.text = gamecontroller.GetPlayerSide();
+        button.interactable = false;
+        gamecontroller.endTurn();
+    }
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        this.gamecontroller = controller;
+    }
+}
