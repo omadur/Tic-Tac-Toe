12f682c1ab456e7d1361a282acbefcadbbde5695
omadur19 <omarduran1989@hotmail.com>
2018-09-08 11:54:01 -0500

    Boton de reinicio
    Se configura el boton de reinicio y se empieza con la configuracion del tablero del jugador que se encuentra en turno para jugar.

--- Tic-Tac-toe-Unity/Assets/Scripts/GameController.cs
+++ Tic-Tac-toe-Unity/Assets/Scripts/GameController.cs
@@ -11,12 +11,21 @@ public class GameController : MonoBehaviour {
     public GameObject gameOverPanel;
     public Text gameOverText;
 
+    private int moveCount;
+
+    private bool winner = false;
+
+    public GameObject restartButton;
+
 
     private void Awake()
     {
         playerSide = "X";
         SetGameControllerReferenceOnButtons();
         gameOverPanel.SetActive(false);
+        moveCount = 0;
+        restartButton.SetActive(false);
+        
     }
 
     void SetGameControllerReferenceOnButtons()
@@ -39,48 +48,91 @@ public class GameController : MonoBehaviour {
 
     public void endTurn()
     {
+        moveCount++;
+
         if(buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
-        ChangeSide();
+
+        if (moveCount == 9 && winner == false)
+        {
+            GameOver("draw");
+        }
+
+            ChangeSide();
     }
 
-    void GameOver()
+    void GameOver(string winningPlayer)
+    {
+        SetBoardInteractable(false);
+        winner = true;
+
+        if(winningPlayer == "draw")
+        {
+            setGameOverText("It's a draw!");
+        }
+        else
+        {
+            setGameOverText(winningPlayer + " Wins!");
+        }
+        restartButton.SetActive(true);
+    }
+
+    void setGameOverText(string value)
+    {
+        gameOverPanel.SetActive(true);
+        gameOverText.text = value;
+    }
+
+    public void RestartGame()
+    {
+        playerSide = "X";
+        moveCount = 0;
+        gameOverPanel.SetActive(false);
+        SetBoardInteractable(true);
+        restartButton.SetActive(false);
+        winner = false;
+
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].text = "";
+        }
+    }
+
+    void SetBoardInteractable(bool toggle)
     {
         for (int i = 0; i < buttonList.Length; i++)
         {
-            buttonList[i].GetComponentInParent<Button>().interactable = false;
-            gameOverPanel.SetActive(true);
-            gameOverText.text = playerSide + " Wins!";
+            buttonList[i].GetComponentInParent<Button>().interactable = toggle;
         }
     }
 }
